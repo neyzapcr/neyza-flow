cd0b2e002635d8276e5f7e124f61a55c6f808331
neyza <neyza24si@mahasiswa.pcr.ac.id>
2026-04-04 23:52:42 +0700

    tugas ney pfl tes 04/04/2026 23:52

---
+++ src/pertemuan-3/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import BeasiswaForm from "./BeasiswaForm";
+import "./tailwind.css";
+
+createRoot(document.getElementById("root")).render(
+  <div>
+    <BeasiswaForm />
+  </div>
+);
